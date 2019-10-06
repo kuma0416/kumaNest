75b5ec7706f3f413ebbea7f19b25d85e894e55ff
kuma0416 <a24350845@gmail.com>
2019-10-06 21:55:30 +0800

    loginAndRegisterDone

---
+++ db.js
@@ -0,0 +1,61 @@
+var MongoClient = require('mongodb').MongoClient;
+
+exports.ConnectAndGet = function ConnectAndGet(callback){
+  MongoClient.connect("mongodb://localhost:27017", function(err, client){
+    var db = client.db('test');
+    if (err) throw err;
+    db.collection('Items', function(err, collection){
+      collection.find().toArray(function(err, items){
+        if (err) throw err;
+        callback(items);
+        client.close();
+      });
+    });
+  });
+}
+//註冊功能
+exports.WriteData = function WriteData(account, password, username, callback){
+  MongoClient.connect("mongodb://localhost:27017", function(err, client){
+    var db = client.db('test');
+    if (err) throw err;
+    db.collection('userspace', function(err, collection){
+      collection.findOne({account:account})//因為檢查重複，所以如果資料庫裏面有，那麼必定只有一筆
+        .then(function(user){//抓取promise成功的資訊
+          if(user){//如果有資料，代表這個帳號在資料庫已經被使用過
+            callback("exist!");//回傳已存在訊息回index.js
+          } else {
+            userInsert(account, password, username);//call function
+            callback("insert!")//回傳註冊成功資料回index.js
+          }
+        })
+    });
+    client.close();//關閉client
+  });
+}
+//插入註冊資料進資料庫
+function userInsert (account,password,username){
+  MongoClient.connect("mongodb://localhost:27017", function(err, client){
+    var db = client.db('test');
+    if (err) throw err;
+    db.collection('userspace', function(err, collection){
+      collection.insert({account:account,password:password,username:username});
+    });
+  });
+}
+//登入
+exports.loginCheck = function loginCheck(account, password, callback){
+  MongoClient.connect("mongodb://localhost:27017", function(err, client){
+    var db = client.db('test');
+    if (err) throw err;
+    db.collection('userspace', function(err, collection){
+      collection.findOne({account: account})
+        .then(function(check){
+          if(account === check.account && password === check.password){
+            callback("success",check.username);
+          } else {
+            callback("fail","");
+          }
+        })
+    });
+  });
+}
---
+++ index.js
@@ -0,0 +1,122 @@
+//var MongoClient=require('mongodb').MongoClient;
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+var db = require('./db');
+const port = 3000;
+var session = require('express-session');
+
+function render(filename, params) {
+  var data = fs.readFileSync(filename, 'utf8');
+  for (var key in params) {
+    data = data.replace('{' + key + '}', params[key]);
+  }
+return data;
+}
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.get('/', function(req, res){
+  var username = req.session.username;
+  res.render('index',{
+    username: username,
+    duplicate: true,
+    login:""
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login',{
+    login:""
+  });
+});
+
+app.get('/reg', function(req, res){
+  res.render('reg',{
+    duplicate: false
+  });
+});
+
+app.post('/reg', function(req, res){
+  console.log(req.body.account + req.body.password + req.body.username);
+  var account = req.body.account;
+  var password = req.body.password;
+  var username = req.body.username;
+  db.WriteData(account, password, username, function(userdata){
+    if(userdata == "exist!"){
+      res.render('reg',{
+        duplicate: true
+      })
+    } else {
+      res.render('index',{
+        login: "",
+        duplicate: false,
+        username: ""
+      })
+    }
+  });
+})
+
+app.post('/login', function(req, res){
+  var account = req.body.account;
+  var password = req.body.password;
+  db.loginCheck(account, password, function(login,username){
+    if(login == "success"){
+      res.render('index',{
+        login: "success",
+        duplicate: true,
+        username: username
+      })
+    } else {
+      res.render('login', {
+        login: "fail"
+      })
+    }
+  });
+});
+
+app.get('/logout', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+});
+app.listen(port);
+ /*
+MongoClient.connect("mongodb://localhost:27017",function(err,client){
+  var db = client.db('test');
+   if(err) throw err;
+   //Write databse Insert/Update/Query code here..
+ 
+   db.collection('Persons',function(err,collection){
+    collection.insert({ id:1, firstName:'Steve', lastName:'Jobs' });
+    collection.insert({ id:2, firstName:'Bill', lastName:'Gates' });
+    collection.insert({ id:3, firstName:'James', lastName:'Bond' });
+ 
+    collection.count(function(err,count){
+        if(err) throw err;
+        console.log('Total Rows:'+count);
+    });
+  });
+  client.close(); //關閉連線
+});
+
+db.ConnectAndGet(function(items){
+  console.log(items);
+});
+
+var username = 'parker';
+db.WriteData(9754814, 31524, username, function(userdata){
+  console.log(userdata);
+});
+*/
